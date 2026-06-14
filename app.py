2a885c22cccb5fa416c056dd245d4ed0f7a8426d
anonimo3967-alt <anonimo3967@gmail.com>
2026-06-13 23:13:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,8 @@ st.markdown(
     f'<h1 style="color:{custom_color}; text-align:center;">Benevolencia</h1>', 
     unsafe_allow_html=True
 )
+
+st.markdown(
+    f'<h3 style="color:{custom_color}; text-align:center;">Feliz día del padre</h3>', 
+    unsafe_allow_html=True
+)
