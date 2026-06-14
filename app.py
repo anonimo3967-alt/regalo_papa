e6fe83c39daa5b0f8ecd9b9370238870ec1c4cad
anonimo3967-alt <anonimo3967@gmail.com>
2026-06-13 23:11:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.image("Benevolence-sol.png")
 
-custom_color = "#FF5733"
+custom_color = "#FFF4CB"
 st.markdown(
     f'<h1 style="color:{custom_color}; text-align:center;">Benevolencia</h1>', 
     unsafe_allow_html=True
