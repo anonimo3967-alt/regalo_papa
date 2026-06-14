536df26c3c0c31588c7fb7012aeedf927816d31e
anonimo3967-alt <anonimo3967@gmail.com>
2026-06-13 23:09:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ st.image("Benevolence-sol.png")
 
 custom_color = "#FF5733"
 st.markdown(
-    f'<h1 style="color:{custom_color}; text-align:'center'">Benevolencia</h1>', 
+    f'<h1 style="color:{custom_color}; text-align:center;">Benevolencia</h1>', 
     unsafe_allow_html=True
 )
